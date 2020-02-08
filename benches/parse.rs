5bcb55e1649f1d6aa64c4b1c3af645128366fe9a
Riey <creeper844@gmail.com>
2020-02-08 17:15:34 +0900

    Add long bench

--- benches/parse.rs
+++ benches/parse.rs
@@ -13,12 +13,29 @@ pub fn throughput_short_bench(c: &mut Criterion) {
     });
     group.bench_with_input("parse", &input, |b, i| {
         b.iter(|| {
-            let insts: Vec<_> = parse(lex(i));
+            let insts = parse(lex(i));
             assert!(!insts.is_empty());
         });
     });
 }
 
-criterion_group!(benches, throughput_short_bench);
+pub fn throughput_long_bench(c: &mut Criterion) {
+    let input = "1 2 + -> $1 $1 { 123 } 그외 { 선택 $1 { 2 { } 그외 { } } }".repeat(100);
+    let mut group = c.benchmark_group("throughput-long");
+    group.throughput(Throughput::Bytes(input.len() as u64));
+    group.bench_with_input("lex", &input, |b, i| {
+        b.iter(|| {
+            assert_eq!(lex(i).count(), 2200);
+        });
+    });
+    group.bench_with_input("parse", &input, |b, i| {
+        b.iter(|| {
+            let insts = parse(lex(i));
+            assert!(!insts.is_empty());
+        });
+    });
+}
+
+criterion_group!(benches, throughput_short_bench, throughput_long_bench);
 criterion_main!(benches);
 
