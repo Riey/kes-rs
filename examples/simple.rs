42f38bb6f39690d8be48957b61c1ced73a1f20e7
Riey <creeper844@gmail.com>
2020-02-09 15:15:38 +0900

    Add example

---
+++ examples/simple.rs
@@ -0,0 +1,14 @@
+use kes::Interpreter;
+use kes::printer::DummyPrinter;
+use bumpalo::Bump;
+
+fn main() {
+    let bump = Bump::with_capacity(8196);
+    let mut interpreter = Interpreter::new(&bump);
+    interpreter.load_script("foo", "1 2 + 3 - 0 <> '1' [-]");
+
+    for _ in 0..10000 {
+        interpreter.run_script("foo", &mut DummyPrinter);
+    }
+}
+
