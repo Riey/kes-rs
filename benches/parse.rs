ed5334527de67dc11fb8897370cfbbfaa2dc31dc
Riey <creeper844@gmail.com>
2020-02-12 20:49:52 +0900

    Update bench

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,21 +1,15 @@
 use bumpalo::Bump;
 use criterion::*;
-use kes::lexer::lex;
 use kes::parser::parse;
 
 pub fn throughput_short_bench(c: &mut Criterion) {
     let input = "1 2 3 4 5 6 7 8 9 '1' '2' '3' '4' '5' '6' '7' '8' '9' 정리";
     let mut group = c.benchmark_group("throughput-short");
     group.throughput(Throughput::Bytes(input.len() as u64));
-    group.bench_with_input("lex", &input, |b, i| {
-        b.iter(|| {
-            assert!(lex(i).count() > 0);
-        });
-    });
     group.bench_with_input("parse", &input, |b, i| {
         let mut bump = Bump::with_capacity(8196);
         b.iter(|| {
-            let insts = parse(&bump, lex(i));
+            let insts = parse(&bump, i);
             assert!(!insts.is_empty());
             drop(insts);
             bump.reset();
@@ -28,15 +22,10 @@ pub fn throughput_long_bench(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("throughput-long");
     group.throughput(Throughput::Bytes(input.len() as u64));
-    group.bench_with_input("lex", &input, |b, i| {
-        b.iter(|| {
-            assert!(lex(i).count() > 0);
-        });
-    });
     group.bench_with_input("parse", &input, |b, i| {
         let mut bump = Bump::with_capacity(1024 * 1024);
         b.iter(|| {
-            let insts = parse(&bump, lex(i));
+            let insts = parse(&bump, i);
             assert!(!insts.is_empty());
             drop(insts);
             bump.reset();
