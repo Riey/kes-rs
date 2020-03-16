0252467af2cd28dcebb9f99cbcf2004867deadc6
Riey <creeper844@gmail.com>
2020-03-16 15:59:40 +0900

    Fix Error

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,24 @@
+use std::fmt::Debug;
 use thiserror::Error;
 
-#[derive(Clone, Debug, Error)]
+#[derive(Clone, Error)]
 pub enum ParserError {
-    #[error("코드해석중 `{1}`번째 줄에서 에러가 발생했습니다 `{0}`")]
+    #[error("코드해석중 {1}번째 줄에서 에러가 발생했습니다 {0}")]
     InvalidCode(&'static str, usize),
-    #[error("잘못된 문자 `{0}`가 `{1}`번째 줄에서 발견됐습니다")]
+    #[error("잘못된 문자 {0}가 {1}번째 줄에서 발견됐습니다")]
     InvalidChar(char, usize),
-    #[error("예상치 못한 토큰 `{0}`가 `{1}`번째 줄에서 발견됐습니다")]
+    #[error("예상치 못한 토큰 {0}가 {1}번째 줄에서 발견됐습니다")]
     UnexpectedToken(String, usize),
-    #[error("컴파일중 `{0}`번째 줄에서 에러가 발생했습니다")]
+    #[error("컴파일중 {0}번째 줄에서 에러가 발생했습니다")]
     CompileError(usize),
     #[error("예상치 못하게 코드가 끝났습니다")]
     UnexpectedEndOfToken,
+}
+
+impl Debug for ParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
 }
 
 pub type ParserResult<T> = Result<T, ParserError>;
